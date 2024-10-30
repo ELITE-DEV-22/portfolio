aeae77b7838ce2e4ab83efa025edf62a91033a95
ELITA <gonsalveselita05@gmail.com>
2024-10-31 02:26:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Landingpage from "./components/Landingpage";
 import Aboutme from './components/Aboutme';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
-import Contact from './components/contact';
+
 
 function App() {
   return (
