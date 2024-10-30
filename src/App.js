b285c8c6dbb35c49ab58dfcdd2c95ae6e4f66fc2
ELITA <gonsalveselita05@gmail.com>
2024-10-31 02:24:56 +0530

    Revert "Setup Routing"
    This reverts commit 85165bd2b91a19cde719500437854c09d9298ad4.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landingpage from './components/Landingpage';
+import Landingpage from "./components/Landingpage";
 import Aboutme from './components/Aboutme';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Contact from './components/contact';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        {/* Home route with Landingpage and Aboutme sections */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Landingpage />
-              <Aboutme />
-            </>
-          }
-        />
-        {/* Separate routes for Projects and Contact pages */}
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <><Navbar/><Landingpage /><Aboutme/><Projects/></>
   );
 }
+/* hello world */ 
 
 export default App;
